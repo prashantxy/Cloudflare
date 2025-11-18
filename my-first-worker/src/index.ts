49841c53bcc05157a08832ef273261e84ecca85e
Prashant Dubey <pdubey1924@gmail.com>
2025-11-19 01:18:30 +0530

    Done

--- my-first-worker/src/index.ts
+++ my-first-worker/src/index.ts
@@ -1,26 +1,29 @@
 
 //Adding hono framework to your cloudflare worker.
-
 import { Hono } from 'hono';
 
+
 // initialize  Hono
-const app = new Hono<{Bindings:Env}>();
+const app = new Hono <{Bindings : Env}>();
 
 //add a simple get request
-app.get('/', c => {
-	return c.json({message : "Hello World"})
-});
 
-//add a simple post request
+app.get('/',async c =>{
+	return c.json({message : "Testing the wrangler worker of cloudflare"})
+})
 
-app.post('/', async c =>{
+//add a simple post request for any product dynamically
+app.post('/',async c=>{
 	const body = c.req.json();
 	const name = body.name;
 	const price = body.price;
-	return c.json(`Created Product ${name} with price ${price}`)
+
+	return c.json(`the name is ${name} and the price is ${price}`);
 })
 
-
+//export the app
 export default app;
+
+//Check on localhost:8787
 
 
